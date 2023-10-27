c3e5bea5c0ff32fabe797e8362f1ef5a1578ad24
Nightycake <michalmarjak@gmail.com>
2023-10-27 14:42:21 +0200

    Init Express App with typescript

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.use('/', (req, res) => {
+  res.send('działa')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
