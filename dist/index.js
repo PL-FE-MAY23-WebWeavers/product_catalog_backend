119acad9651227590502db995e2175da8575d916
Andrzej Pietkiewicz <128551522+anpiet@users.noreply.github.com>
2023-10-29 12:16:31 +0100

    Ci test (#2)
    * add ci test on github & few fixex in lint config
    
    * change husky config
    
    * change husky conf
    
    * change eslint conf
    
    * change package.json
    
    * change package.json, index.js

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+  return (mod && mod.__esModule) ? mod : { 'default': mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = __importDefault(require('express'));
 const app = (0, express_1.default)();
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send('działa ale bez swc oraz swc-node');
+  res.send('działa ale bez swc oraz swc-node');
 });
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
