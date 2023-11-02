940061f82fd34f54c3e111e28f8cedb77dd067b5
Michał Marjak <77036278+Nightycake@users.noreply.github.com>
2023-11-02 12:04:55 +0100

    Products endpoint with pagination (#7)
    * add products endpoint with pagination
    
    * fix endpoints with pagination
    
    * fix conflicts
    
    ---------
    
    Co-authored-by: Nightycake <michalmarjak@gmail.com>

---
+++ src/controller/productsController.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { getPaginatedProducts } from '../service/productsService';
+
+const productsRouter = express.Router();
+
+productsRouter.get('/products', (req, res) => {
+    const page = parseInt(req.query.page as string) || 1;
+    const itemsPerPage = parseInt(req.query.itemsPerPage as string) || 5;
+
+    const result = getPaginatedProducts(page, itemsPerPage);
+
+    res.json(result);
+});
+
+export default productsRouter;
